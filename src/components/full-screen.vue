c21a371400f5b842576f2fdcb9e41caaf2d7801f
臧豪 <zanghao_2000@163.com>
2022-08-27 01:16:07 +0800

    build(依赖): 添加vueUse

---
+++ src/components/full-screen.vue
@@ -0,0 +1,11 @@
+<template>
+    <button @click="toggle">全屏测试</button>
+</template>
+    
+<script setup lang='ts'>
+import { useFullscreen } from '@vueuse/core';
+const { toggle } = useFullscreen()
+</script>
+    
+<style>
+</style>
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,13 @@
 <template>
     <h1>首页</h1>
     <ToDoList />
+    <FullScreen />
+
 </template>
     
 <script setup lang='ts'>
 import ToDoList from '../components/todo-list.vue';
+import FullScreen from '../components/full-screen.vue';
 
 </script>
     
